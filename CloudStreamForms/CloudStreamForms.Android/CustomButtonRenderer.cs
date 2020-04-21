efe21217b84834885dba8a17c70c7c29270c3979
LagradOst <balt.758@gmail.com>
2020-04-21 19:53:40 +0200

    Scrollbar fix buttons

---
+++ CloudStreamForms/CloudStreamForms.Android/CustomButtonRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Button), typeof(CustomButtonRenderer))]
+public class CustomButtonRenderer : ButtonRenderer
+{
+    private Context context;
+    public CustomButtonRenderer(Context context) : base(context)
+    {
+        this.context = context;
+    }
+
+    protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+    {
+        base.OnElementChanged(e);
+        e.NewElement.TextColor = Color.FromHex("e6e6e6");
+    }
+}
